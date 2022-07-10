94b0fb2e24acc74b64bd020e0b22dba1828b78e8
David Gugea <davidgugea@yahoo.com>
2022-07-10 11:20:46 +0200

    Finished Chapter 25

---
+++ Chapter25/nodeWindows.js
@@ -0,0 +1,16 @@
+const {
+    Service
+} = require('node-windows');
+
+
+const service = new Service({
+    name: 'Node.js Webserver',
+    description: 'Webserver listening on port 80.',
+    script: 'C:\\srv\\app\\index.js',
+});
+
+service.on('install', () => {
+    service.start();
+});
+
+service.install();
