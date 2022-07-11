9ed12195c810f85949033de6a55a3d6a9e904636
David Gugea <davidgugea@yahoo.com>
2022-07-11 18:44:11 +0200

    Finished Chapter 27

---
+++ Chapter27/user/connect.js
@@ -0,0 +1,26 @@
+import {
+    connect
+} from 'amqplib';
+
+let channel = null;
+export const queue = 'user';
+
+export async function getChannel() {
+    try {
+        if (channel) {
+            return channel;
+        }
+
+        const connection = await connect('amqp://rabbitmq');
+        channel = await connection.createChannel();
+
+        const ok = await channel.assertQueue(queue);
+
+        if (ok) {
+            return channel;
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
